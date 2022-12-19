741591024475926083ab085e92c491dffcb45bd5
Aiiii-den <becker.work@web.de>
2022-12-19 11:00:43 +0100

    added TowerTank subclass to Aquarium class

--- KotlinBootcampForProgrammers/src/main/kotlin/example/myapp/main.kt
+++ KotlinBootcampForProgrammers/src/main/kotlin/example/myapp/main.kt
@@ -13,10 +13,18 @@ fun buildAquarium() {
     val aquarium4 = Aquarium(width = 25, height = 35, length = 110)
     aquarium4.printSize()
 
+    //Constructor part of tutorial
     val aquarium5 = Aquarium(numberOfFish = 29)
     aquarium5.printSize()
     aquarium5.volume = 70
     aquarium5.printSize()
+
+    //Subclass part
+    val aquarium6 = Aquarium(length = 25, width = 25, height = 40)
+    aquarium6.printSize()
+
+    val myTower = TowerTank(diameter = 25, height = 40)
+    myTower.printSize()
 }
 
 fun main() {
